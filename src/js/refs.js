382c3812aab007c504cb61870d8649c7a3358f71
Volodymyr Klochan <vovkis13@gmail.com>
2021-09-27 18:09:24 +0300

    hw11 color switcher ready

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  bodyRef: document.querySelector('body'),
+  startBtnRef: document.querySelector('button[data-action="start"]'),
+  stopBtnRef: document.querySelector('button[data-action="stop"]'),
+};
